044fce18cf116c080d8d24f8568b65ad92d03967
waysli <58322379@qq.com>
2017-02-23 20:28:15 +0800

    调整api使用注释

--- src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/controller/IndexController.java
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/controller/IndexController.java
@@ -1,5 +1,5 @@
 /**
- * Spring Framework Configuration Files
+ * Index Controller, project entrance
  */
 package ${package}.${artifactId}.controller;
 
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import io.swagger.annotations.ApiOperation;
+
 /**
  * 服务首页
  * @author wangyongsheng
@@ -19,16 +21,19 @@ import java.io.IOException;
 @RequestMapping("/")
 public class IndexController {
 
+    @ApiOperation(value = "首页跳转", notes = "无需参数")
     @RequestMapping(value = "", method = RequestMethod.GET)
     public void home(HttpServletResponse response) throws IOException {
         response.sendRedirect("/swagger-ui.html");
     }
 
+    @ApiOperation(value = "查看状态", notes = "无需参数")
     @RequestMapping(value = "/status", method = RequestMethod.GET)
     public Object status() throws IOException {
         return "ok!";
     }
 
+    @ApiOperation(value = "查看项目信息", notes = "无需参数")
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public Object info() throws IOException {
         return "App Name: " + "${package}.${artifactId}";
