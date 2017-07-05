f46c1cbf65a51908a4f8651573ce64756582543e
waysli <58322379@qq.com>
2017-07-05 16:31:48 +0800

    添加优雅关停服务的功能

--- src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/Application.java
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/Application.java
@@ -2,9 +2,12 @@ package ${package}.${artifactId};
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.xiaojukeji.base.common.util.RestUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -15,5 +18,11 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         logger.info("SpringBoot Start Success!");
+    }
+    
+    @Bean
+    @LoadBalanced
+    RestUtil restOperation() {
+        return new RestUtil();
     }
 }
--- src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/controller/IndexController.java
+++ src/main/resources/archetype-resources/src/main/java/__packageInPathFormat__/__artifactId__/controller/IndexController.java
@@ -6,10 +6,17 @@ package ${package}.${artifactId}.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import com.xiaojukeji.base.common.util.RestUtil;
 import io.swagger.annotations.ApiOperation;
 
 /**
@@ -20,6 +27,14 @@ import io.swagger.annotations.ApiOperation;
 @RestController
 @RequestMapping("/")
 public class IndexController {
+    @Autowired
+    RestUtil restOpt;
+    @Autowired
+    private EurekaClient discoveryClient;
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+    @Value("${spring.application.name}")
+    private String applicationName;
 
     @ApiOperation(value = "首页跳转", notes = "无需参数")
     @RequestMapping(value = "", method = RequestMethod.GET)
@@ -37,5 +52,34 @@ public class IndexController {
     @RequestMapping(value = "/info", method = RequestMethod.GET)
     public Object info() throws IOException {
         return "App Name: " + "${package}.${artifactId}";
+    }
+
+    @ApiOperation(value = "查看当前服务所有实例的状态", notes = "无需参数")
+    @RequestMapping(value = "/getServiceInstances", method = RequestMethod.GET)
+    public Object getServiceInstances() throws Exception {
+        // 找不到对应的服务名，默认返回Collections.emptyList()
+        List<InstanceInfo> instances = discoveryClient.getInstancesByVipAddress(applicationName, true);
+        // 按照instanceId递增排列，id最小的在前面，设为master
+        instances.sort(Comparator.comparing(InstanceInfo::getInstanceId));
+
+        return instances;
+    }
+
+    @ApiOperation(value = "优雅地关闭当前服务实例", notes = "无需参数")
+    @RequestMapping(value = "/graceShutdown", method = RequestMethod.GET)
+    public Object graceShutdown() throws Exception {
+        // 找不到对应的服务名，默认返回Collections.emptyList()
+        List<InstanceInfo> instances;
+        instances = discoveryClient.getInstancesById(instanceId);
+        if (instances.size() > 0) {
+            instances.get(0).setStatus(InstanceInfo.InstanceStatus.DOWN);
+        }
+        discoveryClient.shutdown();
+
+        instances = discoveryClient.getInstancesByVipAddress(applicationName, true);
+        // 按照instanceId递增排列，id最小的在前面，设为master
+        instances.sort(Comparator.comparing(InstanceInfo::getInstanceId));
+
+        return instances;
     }
 }
